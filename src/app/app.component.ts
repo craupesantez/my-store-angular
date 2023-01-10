64fe7b6bfc587a6a1976f5429d1f84e00ed1492a
Cesar <craupesantez@gmail.com>
2023-01-09 20:53:56 -0500

    ejemplo de input y output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,78 +6,9 @@ import { Product } from './product.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  widthImg = 10;
-  name = 'Cesar';
-  age = 18;
-  img = 'https://www.w3schools.com/howto/img_avatar.png';
-  btnDisabled = true;
-  names: string[] = ['cesar', 'carlos', 'mireya'];
-  newName = '';
-  register = {
-    name: '',
-    email:'',
-    password: '',
-  }
-  box= {
-    width: 100,
-    height: 100,
-    background: 'red'
-  }
-  products : Product[]= [
-    {
-      name: 'el mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg',
-      category:'all'
-    },
-    {
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      name: 'Colleccion de albumes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    }
-  ]
+ imgParent = '';
 
-  person ={
-    name: 'cesar',
-    age : 18,
-    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
-  }
-
-  toggleButton(){
-    this.btnDisabled = !this.btnDisabled;
-  }
-
-  increasseAge(){
-    this.person.age+= 1;
-  }
-  onScroll(event: Event){
-    const element = event.target as HTMLElement;
-    console.log(element.scrollTop)
-  }
-
-  changeName(event: Event){
-    const element = event.target as HTMLInputElement;
-    this.person.name = element.value;
-  }
-
-  addName(){
-    this.names.push(this.newName);
-    this.newName='';
-  }
-  deleteName(index: number){
-    this.names.splice(index, 1);
-  }
-  onRegister(){
-    console.log(this.register);
-  }
+ onLoaded(img :string){
+  console.log('log padre', img)
+ }
 }
---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss']
+})
+export class ImgComponent {
+
+  @Input() img: string= '';
+  @Output() loaded = new EventEmitter<string>();
+  imageDefault = './assets/images/default.jpg'
+
+  imgError(){
+    this.img = this.imageDefault;
+  }
+  imgLoaded(){
+    console.log('log hijo');
+    this.loaded.emit(this.img);
+  }
+}
